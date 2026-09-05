078d65475efe2e026a30c4fc08efda76c196e76e
noodleybroodley <educatedgraph@gmail.com>
2026-09-05 10:40:19 -0500

    updated repo

---
+++ README.md
@@ -0,0 +1,38 @@
+# camb.io: Spotify to Apple Music Playlist Converter
+<img width="944" alt="image" src="./client/public/titleCard.png">
+
+## Link: https://cambio.onrender.com/
+
+## Description:
+
+A React App that takes a Spotify Playlist URL and transfers the playlist to the user's Apple Music Library.
+
+## Run the Project Locally:
+
+### Warning: To run this app locally, you'll first need to set up developer accounts for both Apple and Spotify. Uma Abu has a great video on how to do this (https://www.youtube.com/watch?v=mS2J0dKcvAA&t=1s).
+1. After cloning the project repo, run the command `yarn` in both the `client` and `server` directories.
+2. Create an .env file in the server directory with both the Apple and Spotify client credentials (example below).
+<br/>
+   <img width="416" alt="image" src="./client/public/server_env.png">
+3. Create an .env file in the client directory with the backend route stored in it (example below).
+<br/>
+   <img width="416" alt="image" src="./client/public/client_env.png">
+4. Run command `yarn start` in the `server` directory and secondly in the `client` directory.
+5. Open http://localhost:3000 to view it in your browser.
+
+### Note: I've noticed some incompatibility issues with the Chrome plugin `Night Mode Pro`, I'm working to resolve this.
+
+## Converting a Playlist:
+
+1. Ensure the Spotify playlist is public, then copy and paste the playlist URL into the search bar.
+2. Press enter. (A playlist card should appear along with a new text field)
+<img alt="image" src="./client/public/playlistCard.png">
+3. Either create a new playlist name with the text field or press enter (defaults to Spotify playlist name).
+4. Authenticate with your Apple Music account.
+5. When Transfer is complete, a pop up appears that displays the tracks that couldn't be found.
+<img alt="image" src="./client/public/transferComplete.png">
+6. The playlist will now be in the user's Apple Music Library!
+<img alt="image" src="./client/public/appleMusic.png">
+
+## Credits:
+Many thanks to Uma Abu, his playlist converter project was invaluable to creating this application! Check it out at https://github.com/ueabu/spotify-to-apple-music.
---
+++ server/index.js
@@ -0,0 +1,165 @@
+const express = require('express');
+const app = express();
+const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
+
+const cors = require('cors');
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+].filter(Boolean);
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+            return;
+        }
+
+        callback(new Error('Not allowed by CORS'));
+    }
+}));
+
+let spotifyApi = new SpotifyWebApi({
+    clientId: `${process.env.REACT_APP_ID}`,
+    clientSecret: `${process.env.REACT_APP_SECRET}`,
+    redirectUri: `${process.env.FRONTEND_URL || 'http://localhost:3000'}/`
+});
+
+app.get("/api/login", (req, res) => {
+    /** Reaches out to Spotify API and performs client credentials grant,
+     * then sets the access token for the Spotify Web API instance.*/
+    spotifyApi.clientCredentialsGrant().then(
+        function (data) {
+            console.log('The access token expires in ' + data.body['expires_in']);
+            console.log('The access token is ' + data.body['access_token']);
+
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+        },
+        function (err) {
+            console.log(
+                'Something went wrong when retrieving an access token',
+                err.message
+            );
+        })
+    res.json(spotifyApi);
+});
+app.get("/api/logout", (req, res) => {
+    /** Removes Access Token from Spotify Web API instance.*/
+    spotifyApi.setAccessToken("");
+    res.json(spotifyApi);
+})
+
+function extractSongName(str) {
+    /** Takes a raw song name string and returns just the song title without featured artists
+     * Param:
+     * str: String
+     *
+     * Return:
+     * String*/
+    if (str.includes('(feat. ')) {
+        return str.substring(0, str.indexOf('(feat. '))
+    }
+    else if (str.includes('(Feat. ')) {
+        return str.substring(0, str.indexOf('(Feat. '))
+    }
+    else if (str.includes('(with ')) {
+        return str.substring(0, str.indexOf('(with '))
+    }
+    return str
+}
+
+
+async function getAllSongs(id) {
+    /** Gets all tracks from a given Spotify playlist.
+     * Param:
+     * id: String
+     *
+     * Return:
+     * {trackName: String, artistName: String[], albumName: String, albumArtist: String[]}[]
+     * */
+    var data = await spotifyApi.getPlaylistTracks(id);
+    var numBatches = Math.floor(data.body.total / 100) + 1;
+    var promises = [];
+    var songs = [];
+
+    for (let batchNum = 0; batchNum < numBatches; batchNum++) {
+        //Create an array of promises that will process and structure song data
+        var promise = getSongs(id, batchNum * 100).then(data=>{
+            data.body.items.forEach((data) => {
+                let cur = {};
+                if (data.track !== null) {
+                    cur["trackName"] = extractSongName(data.track.name);
+                    cur["artistName"] = [];
+                    data.track.artists.forEach((artist) => {
+                        cur["artistName"].push(artist.name)
+                    })
+                    // add album parameters
+                    cur["albumName"] = data.track.album.name
+                    cur["albumArtist"] = []
+                    data.track.album.artists.forEach((artist) => {
+                        cur["albumArtist"].push(artist.name)
+                    })
+                    songs.push(cur)
+                }
+            })
+        });
+        promises.push(promise);
+    }
+    var finalPromise = await Promise.all(promises);
+    
+    return songs;
+}
+
+async function getSongs(id, offset) {
+    /** Uses Spotify Web API to get the raw data for each song in a playlist
+     * Params:
+     * id: String
+     * offset: Number}
+     *
+     * Return:
+     * Object*/
+    var songs = await spotifyApi.getPlaylistTracks(id, { offset: offset });
+    return songs;
+}
+app.get("/api/getPlaylist/:id", async (req, res) => {
+    /** Backend endpoint for getting all songs in a playlist*/
+    var songs = await getAllSongs(req.params.id).catch(err => err);
+    var playlist = await spotifyApi.getPlaylist(req.params.id).catch(err => err);
+    let message = playlist.body?.error ? `request failed because of ${playlist.body?.error.message}` : "got playlist successfully";
+    console.log("hit getPlaylist endpoint, ",message);
+    res.send([songs, playlist]);
+})
+
+//Apple Music
+
+const jwt = require('jsonwebtoken')
+
+const private_key = `${process.env.APPLE_MUSIC_SECRET}`
+const team_id = `${process.env.APPLE_MUSIC_TEAM_ID}`
+const key_id = `${process.env.APPLE_MUSIC_ID}`
+
+//Use private key, team id and key id to get Apple Music Auth Token
+const token = jwt.sign({}, private_key, {
+    algorithm: 'ES256',
+    expiresIn: '180d',
+    issuer: team_id,
+    header: {
+        alg: 'ES256',
+        kid: key_id
+    }
+})
+
+app.get('/api/appleToken', (req, res) => {
+    /** Endpoint to retrieve Apple Music Token*/
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ token: token }))
+})
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080")
+})
+
+// module.exports = {spotifyApi};
